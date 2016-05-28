ae18a991294254ae55bcecfe85a64fefd6243288
Pratibha Ladkat <pratibhaladkat@pratibhaladkat.(none)>
2016-05-28 15:27:56 +0530

    task 5 commit

--- phpreboot/tddworkshop/Calculator.php
+++ phpreboot/tddworkshop/Calculator.php
@@ -12,13 +12,16 @@ class Calculator
         if (!is_string($numbers)) {
             throw new \InvalidArgumentException('Parameters must be a string');
         }
-        $numbers = str_replace(array("\r\n","\r","\n","\\r","\\n","\\r\\n"),",",$numbers);
-        $numbersArray = explode(",", $numbers);
-
+        $delimiters = array("\r\n","\r","\n","\\r","\\n","\\r\\n",'\\;','\\',';');
+        
+        $numbers = str_replace($delimiters,",",$numbers);
+        $numbersArray = array_filter(explode(",", $numbers));
         if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
             throw new \InvalidArgumentException('Parameters string must contain numbers');
         }
-
+        if (array_filter($numbersArray, function ($v) {return $v > 0;}) !== $numbersArray) {
+            throw new \InvalidArgumentException('Negative numbers not allowed');
+        }
         return array_sum($numbersArray);
     }
 }
